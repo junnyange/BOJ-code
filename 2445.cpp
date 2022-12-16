f6a4a989266d288e0e0f75a2b349a925bb030787
HeoJunsang <hjs15923@naver.com>
2022-12-16 21:35:03 +0900

    [BOJ] 2445번 : 별찍기 - 8

---
+++ 2445.cpp
@@ -0,0 +1,28 @@
+//HeoJunSang's Code
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    for(int i=1;i<N;i++)
+    {
+        for(int j=1;j<=i;j++) cout << "*";
+        for(int j=2*N-1;j>=i*2;j--) cout << " ";
+        for(int j=1;j<=i;j++) cout << "*";
+        cout << "\n";
+    }
+
+    for(int i=0;i<=2*N-1;i++) cout << "*";
+    cout << "\n";
+
+    for(int i=N-1;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++) cout << "*";
+        for(int j=2*N-1;j>=i*2;j--) cout << " ";
+        for(int j=1;j<=i;j++) cout << "*";
+        cout << "\n";
+    }
+}
